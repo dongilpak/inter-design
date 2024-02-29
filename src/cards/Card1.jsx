d56aee9f6cbac6fae2ff8fc69c66b51083a7c820
dongilpak <ehddlf2395@gmail.com>
2024-02-29 15:24:27 +0900

    card 1

--- src/cards/Card1.jsx
+++ src/cards/Card1.jsx
@@ -1,5 +1,26 @@
+/* eslint-disable react/no-unescaped-entities */
+import './Card1.css';
 const Card1 = () => {
-    return <div>Card1</div>;
+    return (
+        <article className='card1'>
+            <div className='card-container'>
+                <div className='thumb'></div>
+                <div className='infos'>
+                    <h2 className='title'>
+                        NEW YORK CITY <span className='flag'></span>
+                    </h2>
+                    <div className='date'>NOVEMBER 2-4</div>
+                    <div className='seats'>SEATS REMAINING: 2</div>
+                    <p className='txt'>
+                        Join us for our Live Infinity Session in beautiful New
+                        York City. This is a 3 day intensive workshop where
+                        you'll learn how to become a better version of....
+                    </p>
+                    <h3 className='details'>EVENT DETAILS</h3>
+                </div>
+            </div>
+        </article>
+    );
 };
 
 export default Card1;
